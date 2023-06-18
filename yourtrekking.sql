cb07106ae0bfcb75dc91b55ea92ec6ba87535509
KS <kisung.lee@gmail.com>
2023-06-18 21:30:30 +0900

    Create yourtrekking.sql
    등산일정을 기록하고 싶어서

---
+++ yourtrekking.sql
@@ -0,0 +1,36 @@
+DROP TABLE yourtrekking CASCADE;
+
+CREATE TABLE yourtrekking (
+id SERIAL,
+모임종류 varchar(128),
+모임종류_id INTEGER,
+장소 VARCHAR(128),
+동행1 varchar(128),
+동행2 VARCHAR(128),
+동행3 VARCHAR(128),
+언제어디서 VARCHAR(128),
+언제어디서_id INTEGER,
+요일 VARCHAR(128),
+PRIMARY KEY(id)
+);
+
+INSERT INTO yourfriends (모임종류,장소,동행1,동행2,동행3,언제어디서,요일) 
+VALUES
+('좋은사람들', '금원산','-', '정재순', '-', '2023-06-17','일요일');
+
+DROP TABLE 모임종류;
+DROP TABLE 직업;
+DROP TABLE 언제어디서;
+DROP TABLE 기간;
+
+CREATE TABLE 모임종류
+(id SERIAL,
+모임종류 VARCHAR(128),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE 직업
+(id SERIAL,
+직업 VARCHAR(128),
+PRIMARY KEY(id)
+);
